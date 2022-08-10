12fde00e53fd73cbaad2103a172a5b243c248791
ignacio gonzalez <solrock12@gmail.com>
2022-08-09 22:20:23 -0300

    ok

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
